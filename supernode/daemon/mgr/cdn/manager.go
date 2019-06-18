8621feb5a0eefa7bcc21b0a4297269f4800188c5
Starnop <starnopg@gmail.com>
2019-06-18 19:56:46 +0800

    bugfix: parse the lastmodified header to timestamp
    Signed-off-by: Starnop <starnop@163.com>

--- supernode/daemon/mgr/cdn/manager.go
+++ supernode/daemon/mgr/cdn/manager.go
@@ -166,7 +166,7 @@ func (cm *Manager) handleCDNResult(ctx context.Context, task *types.TaskInfo, re
 }
 
 func (cm *Manager) updateLastModifiedAndETag(ctx context.Context, taskID, lastModified, eTag string) {
-	lastModifiedInt, _ := strconv.ParseInt(lastModified, 10, 64)
+	lastModifiedInt, _ := cutil.ConvertTimeStringToInt(lastModified)
 	if err := cm.metaDataManager.updateLastModifiedAndETag(ctx, taskID, lastModifiedInt, eTag); err != nil {
 		logrus.Errorf("failed to update LastModified(%s) and ETag(%s) for taskID %s: %v", lastModified, eTag, taskID, err)
 	}
